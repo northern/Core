d280e198826ef22fab1193933601bb1b22439a06
Luke Schreur <luke.schreur@gmail.com>
2015-12-13 14:11:39 +1100

    Moved ValidationException to ValidationExceptionAwareTrait.

---
+++ src/Northern/Core/Common/Exception/ValidationExceptionAwareTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Northern\Core\Common\Exception;
+
+use Northern\Core\Common\Exception\Validation\Errors;
+
+trait ValidationExceptionAwareTrait {
+
+	protected $errors;
+	protected $values;
+
+	public function __construct( Errors $errors, array $values = array(), \Exception $previous = NULL )
+	{
+		parent::__construct("A validation exception occured.", static::$scope, $previous);
+
+		$this->setErrors( $errors );
+		$this->setValues( $values );
+	}
+
+	public function setErrors( Errors $errors )
+	{
+		$this->errors = $errors;
+	}
+
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+
+	public function setValues( array $values )
+	{
+		$this->values = $values;
+	}
+
+	public function getValues()
+	{
+		return $this->values;
+	}
+
+}
